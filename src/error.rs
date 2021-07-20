ee002d17c910cc7bb128bbf41d0f1c487bbb685d
Jonathan LEI <xJonathan@outlook.com>
2021-07-20 09:18:29 +0000

    Implement file decryption

--- src/error.rs
+++ src/error.rs
@@ -1,10 +1,16 @@
 #[derive(Debug)]
 pub enum EncryptError {
     FileError(std::io::Error),
+    ReadPasswordError(std::io::Error),
     PasswordMismatch,
     RandomnessError(getrandom::Error),
     FileLengthMismatch,
 }
 
 #[derive(Debug)]
-pub enum DecryptError {}
+pub enum DecryptError {
+    FileError(std::io::Error),
+    ReadPasswordError(std::io::Error),
+    FileLengthMismatch,
+    DecryptionFailed(block_modes::BlockModeError),
+}
--- src/run.rs
+++ src/run.rs
@@ -13,7 +13,7 @@ type Aes256Cbc = Cbc<Aes256, Pkcs7>;
 pub fn run_encrypt(path: &str) -> Result<(), EncryptError> {
     let mut file = fs::File::open(path).map_err(EncryptError::FileError)?;
     let password = rpassword::read_password_from_tty(Some("Enter Password: "))
-        .map_err(EncryptError::FileError)?;
+        .map_err(EncryptError::ReadPasswordError)?;
 
     let confirm_password = rpassword::read_password_from_tty(Some("Repeat password: "))
         .map_err(EncryptError::FileError)?;
@@ -22,9 +22,7 @@ pub fn run_encrypt(path: &str) -> Result<(), EncryptError> {
     }
 
     // Generate key from password
-    let mut sha256 = Sha256::new();
-    sha256.update(password.as_bytes());
-    let key: [u8; 32] = sha256.finalize()[..].try_into().unwrap();
+    let key: [u8; 32] = password_to_key(&password);
 
     // Generate IV randomly
     let mut iv: [u8; 16] = [0; 16];
@@ -54,6 +52,45 @@ pub fn run_encrypt(path: &str) -> Result<(), EncryptError> {
     Ok(())
 }
 
-pub fn run_decrypt(_path: &str, _legacy: bool) -> Result<(), DecryptError> {
-    todo!();
+pub fn run_decrypt(path: &str, _legacy: bool) -> Result<(), DecryptError> {
+    let mut file = fs::File::open(path).map_err(DecryptError::FileError)?;
+    let password = rpassword::read_password_from_tty(Some("Enter Password: "))
+        .map_err(DecryptError::ReadPasswordError)?;
+
+    // Generate key from password
+    let key: [u8; 32] = password_to_key(&password);
+
+    // Read IV from file
+    let mut iv: [u8; 16] = [0; 16];
+    file.read(&mut iv).map_err(DecryptError::FileError)?;
+
+    let input_file_meta = file.metadata().map_err(DecryptError::FileError)?;
+    let file_len: usize = input_file_meta.len().try_into().unwrap();
+
+    // TODO: change to encrypt by block
+    let mut buffer = Box::new(vec![0; file_len - iv.len()]);
+    if file.read(&mut buffer).map_err(DecryptError::FileError)? != buffer.len() {
+        return Err(DecryptError::FileLengthMismatch);
+    }
+
+    let cipher = Aes256Cbc::new_from_slices(&key, &iv).unwrap();
+    let plaintext = cipher
+        .decrypt_vec(&buffer)
+        .map_err(DecryptError::DecryptionFailed)?;
+
+    let mut tmp_file = tempfile::NamedTempFile::new().map_err(DecryptError::FileError)?;
+    tmp_file
+        .write(&plaintext[..])
+        .map_err(DecryptError::FileError)?;
+    tmp_file.flush().map_err(DecryptError::FileError)?;
+
+    std::fs::copy(tmp_file.path(), path).map_err(DecryptError::FileError)?;
+
+    Ok(())
+}
+
+fn password_to_key(password: &str) -> [u8; 32] {
+    let mut sha256 = Sha256::new();
+    sha256.update(password.as_bytes());
+    sha256.finalize()[..].try_into().unwrap()
 }
